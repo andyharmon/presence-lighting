75a2c8041f625bda14f09715421e42e488c52f73
Andy Harmon <andyharmon@msn.com>
2020-10-07 12:54:34 +0000

    Add console project and package references

---
+++ presence-lighting/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace presence_lighting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
